b36988313895b0144c43ef9a1fe8e14c16745a25
LiTigre <li.zhang17@mail.mcgill.ca>
2019-10-09 15:47:59 -0400

    solutions to merge after workshop

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request
-# Import your models
+from gpt2_predictor import GPT2_predictor
+from mnist_predictor import MNIST_predictor
 
 
 app = Flask(__name__)
 
-# Instantiate your models
-
+mnist = MNIST_predictor()
+gpt2 = GPT2_predictor()
 
 # Base endpoint to perform prediction.
 @app.route('/', methods=['POST'])
 def make_prediction():
-    return render_template('index.html', prediction=None, generated_text=None, tab_to_show='mnist')
+    if request.form['predictor'] == 'mnist':
+        prediction = mnist.predict(request)
+        return render_template('index.html', prediction=prediction, generated_text=None, tab_to_show='mnist')
+    elif request.form['predictor'] == 'gpt2':
+        prediction = gpt2.predict(request)
+        return render_template('index.html', prediction=None, generated_text=prediction, tab_to_show='gpt2')
 
 
 @app.route('/', methods=['GET'])
